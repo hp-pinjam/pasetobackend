d37b672f66fd2edc521df52cd963f192b9624736
FarhanRiziq01 <farhanriziq01@gmail.com>
2023-12-03 23:30:17 +0700

    update

--- type.go
+++ type.go
@@ -6,31 +6,13 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type User struct {
-	Email        string `bson:"email,omitempty" json:"email,omitempty"`
-	Username     string `json:"username" bson:"username"`
-	Password     string `json:"password" bson:"password"`
-	PasswordHash string `json:"passwordhash" bson:"passwordhash"`
-	Role         string `json:"role,omitempty" bson:"role,omitempty"`
-	Token        string `json:"token,omitempty" bson:"token,omitempty"`
-	Private      string `json:"private,omitempty" bson:"private,omitempty"`
-	Public       string `json:"public,omitempty" bson:"public,omitempty"`
-}
-
 type Admin struct {
-	Email        string `bson:"email,omitempty" json:"email,omitempty"`
-	Username     string `json:"username" bson:"username"`
-	Password     string `json:"password" bson:"password"`
-	PasswordHash string `json:"passwordhash" bson:"passwordhash"`
-	Role         string `json:"role,omitempty" bson:"role,omitempty"`
-	Token        string `json:"token,omitempty" bson:"token,omitempty"`
-	Private      string `json:"private,omitempty" bson:"private,omitempty"`
-	Public       string `json:"public,omitempty" bson:"public,omitempty"`
-}
-
-type RegisterStruct struct {
-	Username string `json:"username" bson:"username"`
+	Email    string `json:"email" bson:"email"`
 	Password string `json:"password" bson:"password"`
+	Role     string `json:"role,omitempty" bson:"role,omitempty"`
+	Token    string `json:"token,omitempty" bson:"token,omitempty"`
+	Private  string `json:"private,omitempty" bson:"private,omitempty"`
+	Public   string `json:"public,omitempty" bson:"public,omitempty"`
 }
 
 type Credential struct {
@@ -45,11 +27,29 @@ type Response struct {
 	Data    interface{} `json:"data" bson:"data"`
 }
 
-type Hp struct {
+type Payload struct {
+	Admin   string    `json:"admin"`
+	Catalog string    `json:"catalog"`
+	Role    string    `json:"role"`
+	Exp     time.Time `json:"exp"`
+	Iat     time.Time `json:"iat"`
+	Nbf     time.Time `json:"nbf"`
+}
+
+type Crawling struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" `
+	Created_at string             `json:"created_at" bson:"created_at"`
+	Full_text  string             `json:"full_text" bson:"full_text"`
+	Username   string             `json:"username" bson:"username"`
+	Location   string             `json:"location" bson:"location"`
+}
+
+type Catalog struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty" `
 	Nomorid     int                `json:"nomorid" bson:"nomorid"`
 	Title       string             `json:"title" bson:"title"`
 	Description string             `json:"description" bson:"description"`
+	Lokasi      string             `json:"lokasi" bson:"lokasi"`
 	Image       string             `json:"image" bson:"image"`
 	Status      bool               `json:"status" bson:"status"`
 }
@@ -62,35 +62,11 @@ type About struct {
 	Status      bool   `json:"status" bson:"status"`
 }
 
-type Detail struct {
-	ID          int    `json:"id" bson:"id"`
-	Title       string `json:"title" bson:"title"`
-	Description string `json:"description" bson:"description"`
-	Cari        string `json:"cari" bson:"cari"`
-	Tanggal     string `json:"tanggal" bson:"tanggal"`
-	Image       string `json:"image" bson:"image"`
-	Harga       int    `json:"harga" bson:"harga"`
-	Hp          []Hp   `json:"hp" bson:"hp"`
-	Rating      string `json:"rating" bson:"rating"`
-	Status      bool   `json:"status" bson:"status"`
-}
-
 type Contact struct {
-	ID      int    `json:"id" bson:"id"`
-	Name    string `json:"title" bson:"title"`
-	Subject string `json:"description" bson:"description"`
-	Alamat  string `json:"alamat" bson:"alamat"`
-	Website string `json:"website" bson:"website"`
-	Message string `json:"image" bson:"image"`
-	Email   string `json:"email" bson:"email"`
-	Phone   string `json:"phone" bson:"phone"`
-	Status  bool   `json:"status" bson:"status"`
-}
-
-type Payload struct {
-	Id   primitive.ObjectID `json:"id"`
-	Role string             `json:"role"`
-	Exp  time.Time          `json:"exp"`
-	Iat  time.Time          `json:"iat"`
-	Nbf  time.Time          `json:"nbf"`
+	ID       int    `json:"id" bson:"id"`
+	FullName string `json:"fullname" bson:"fullname"`
+	Email    string `json:"email" bson:"email"`
+	Phone    string `json:"phone" bson:"phone"`
+	Message  string `json:"image" bson:"image"`
+	Status   bool   `json:"status" bson:"status"`
 }
